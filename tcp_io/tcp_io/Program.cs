b4e780dda5fe14c89030f528e8ea81e50cd5a477
Petrb <benapetr@gmail.com>
2012-06-17 14:36:15 +0200

    new sub project

---
+++ tcp_io/tcp_io/Program.cs
@@ -0,0 +1,16 @@
+﻿// Part of wmib
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcp_io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Connect();
+        }
+    }
+}
