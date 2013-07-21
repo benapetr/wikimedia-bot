51b4afdbd53ddd7350b813b0ad651de96016de9c
Donald J. Fortier II <Technical_13@yahoo.com>
2013-07-21 17:52:28 -0400

    Update Class1.cs
    Don't say "you're welcome" if they say "no thank"*

--- plugins/Thanks/Thanks/Class1.cs
+++ plugins/Thanks/Thanks/Class1.cs
@@ -104,7 +104,7 @@ namespace wmib
             else
             {
                 message = message.ToLower();
-                if (message.Contains(channel.instance.Nick) && !message.Contains("thanks to") && (message.Contains("thanks") || message.Contains("thank you")))
+                if (message.Contains(channel.instance.Nick) && !message.Contains("thanks to") && (message.Contains("thanks") || message.Contains("thank you")) && !message.Contains("no thank"))
                 {
                     string response = "Hey " + invoker.Nick + ", you are welcome!";
                     Buffer.Item x = Ring.getUser(invoker.Nick);
