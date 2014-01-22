e0f3caa5b906fb29ec2e7aee5f8687ba62421a2f
Petr Bena <benapetr@gmail.com>
2014-01-22 15:34:39 +0100

    missing hooks

---
+++ System/SystemHooks.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace wmib
+{
+	public class SystemHooks
+	{
+		public static void IrcReloadChannelConf(config.channel Channel)
+		{
+			lock(Module.module)
+			{
+				foreach (Module xx in Module.module)
+				{
+					try
+					{
+						if (xx.working)
+						{
+							xx.Hook_ReloadConfig(Channel);
+						}
+					} catch (Exception fail)
+					{
+						Syslog.Log("Crash on Hook_Reload in " + xx.Name);
+						core.handleException(fail);
+					}
+				}
+			}
+		}
+
+		public static void IrcKick(config.channel Channel, User Source, User Target)
+		{
+			lock(Module.module)
+			{
+				foreach (Module module in Module.module)
+				{
+					if (!module.working)
+					{
+						continue;
+					}
+					try
+					{
+						module.Hook_Kick(Channel, Source, Target);
+					} catch (Exception fail)
+					{
+						Syslog.Log("MODULE: exception at Hook_Kick in " + module.Name, true);
+						core.handleException(fail);
+					}
+				}
+			}
+		}
+
+		public static void SystemLog(string Message, bool Warning)
+		{
+			return;
+		}
+	}
+}
+
