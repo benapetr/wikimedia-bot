6d730b5cbeddcf91479c3ca38eb089f2f42880f9
Petr Bena <benapetr@gmail.com>
2014-02-12 21:15:51 +0100

    blah

--- easydepl.sh
+++ easydepl.sh
@@ -125,7 +125,7 @@ cp "$source/plugins/Thanks/bin/Debug/Thanks.dll" "$target/modules/thanks.bin" ||
 cp "$source/plugins/wmib_rc/bin/Debug/wmib_rc.dll" "$target/modules/wmib_rc.bin" || exit 1
 cp "$source/plugins/wmib_statistics/bin/Debug/wmib_statistics.dll" "$target/modules/wmib_statistics.bin" || exit 1
 cp "$source/plugins/wmib_rafeed/bin/Debug/wmib_rafeed.dll" "$target/modules/wmib_rafeed.bin" || exit 1
-cp "$source/plugins/wmib_infobot/bin/Debug/wmib_notify.dll" "$target/modules/wmib_notify.bin" || exit 1
+cp "$source/plugins/wmib_notify/bin/Debug/wmib_notify.dll" "$target/modules/wmib_notify.bin" || exit 1
 ok
 text "Restarting the bot"
 rm "$target/restart.lock" || exit 1
