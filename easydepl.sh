e19554c1607cd701dc78f1021a6474caec315eec
Petr Bena <benapetr@gmail.com>
2014-02-12 20:51:02 +0100

    some improvements

--- easydepl.sh
+++ easydepl.sh
@@ -5,7 +5,8 @@ WARN=$(tput setaf 3)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 target=/mnt/share/beta
-source=/mnt/share/wikimedia_bot
+parameters=/verbosity:quiet
+source=/mnt/share/wikimedia-bot
 
 ok()
 {
@@ -26,13 +27,67 @@ text()
 
 
 cd "$source" || exit 1
-echo "Cleaning the source code"
+text "Cleaning the source code"
 
-make forced-clean || exit 1
+make forced-clean > /dev/null || exit 1
 
-echo "Building the new code..."
+ok
+text "Building the new code..."
+make > /dev/null || exit 1
+ok
 
-make || exit 1
+text "Building module wmib_rc"
+cd plugins/wmib_rc || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module wmib_logs"
+cd ../wmib_log || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module htmldump"
+cd ../htmldump || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module blah"
+cd ../slap || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module wmib_notify"
+cd ../wmib_notify || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module seen"
+cd ../seen || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module linkie"
+cd ../Linkie || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module labs"
+cd ../labs || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module op"
+cd ../op || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module thanks"
+cd ../Thanks || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module wmib_infobot"
+cd ../wmib_infobot || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module wmib_rafeed"
+cd ../wmib_rafeed || exit 1
+xbuild > /dev/null || exit 1
+ok
+text "Building module wmib_statistics"
+cd ../wmib_statistics || exit 1
+xbuild > /dev/null || exit 1
+ok
 
 text "Stopping the bot"
 
@@ -56,7 +111,20 @@ done
 ok
 
 text "Updating the binary file"
+
 cp "$source/bin/Debug/wmib.exe" "$target/wmib.exe" || exit 1
+cp "$source/plugins/seen/bin/Debug/plugin.dll" "$target/modules/seen.bin" || exit 1
+cp "$source/plugins/htmldump/bin/Debug/htmldump.dll" "$target/modules/htmldump.bin" || exit 1
+cp "$source/plugins/wmib_log/bin/Debug/wmib_log.dll" "$target/modules/wmib_logs.bin" || exit 1
+cp "$source/plugins/labs/bin/Debug/labs.dll" "$target/modules/labs.bin" || exit 1
+cp "$source/plugins/Linkie/bin/Debug/Linkie.dll" "$target/modules/linkie.bin" || exit 1
+cp "$source/plugins/op/bin/Debug/op.dll" "$target/modules/op.bin" || exit 1
+cp "$source/plugins/slap/bin/Debug/slap.dll" "$target/modules/slap.bin" || exit 1
+cp "$source/plugins/wmib_infobot/bin/Debug/wmib_infobot.dll" "$target/modules/wmib_infobot.bin" || exit 1
+cp "$source/plugins/Thanks/bin/Debug/Thanks.dll" "$target/modules/thanks.bin" || exit 1
+cp "$source/plugins/wmib_rc/bin/Debug/wmib_rc.dll" "$target/modules/wmib_rc.bin" || exit 1
+cp "$source/plugins/wmib_statistics/bin/Debug/wmib_statistics.dll" "$target/modules/wmib_statistics.bin" || exit 1
+cp "$source/plugins/wmib_rafeed/bin/Debug/wmib_rafeed.dll" "$target/modules/wmib_rafeed.bin" || exit 1
 ok
 text "Restarting the bot"
 rm "$target/restart.lock" || exit 1
