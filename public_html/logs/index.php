723bc4a8995c987e86a0ef4c4fd8477174ad8c25
Petr Bena <benapetr@gmail.com>
2014-03-22 10:15:46 +0100

    updated in logs

--- public_html/logs/index.php
+++ public_html/logs/index.php
@@ -2,8 +2,9 @@
 
 // This is interface for wm-bot's logs
 require ("includes/core.php");
+$exec=time();
 
 Logs::Init();
 Logs::Render();
 
-echo "<!-- finished -->";
+echo "<!-- finished in " . time() - $exec . " seconds -->";
