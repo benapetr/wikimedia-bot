3a45c6b22e189f4777426233ff8a05b47a9c7f0e
John Du Hart <john@johnduhart.me>
2014-04-13 12:28:54 -0700

    Fixed linux build scripts

--- build/clean.sh
+++ build/clean.sh
@@ -5,13 +5,9 @@ if [ x"$1" = x"--force" ];then
   force=1
 fi
 
-if [ -d obj ];then
-  rm -rv obj
-fi
-
 if [ -d bin ];then
 if [ $force -eq 0 ];then
-  if [ -d bin/Debug/configuration ] || [ -d bin/Release/configuration ];then
+  if [ -d bin/configuration ];then
     echo "Not removing bin because there is a data folder, use make forced-clean if you really want to delete it"
     exit 1
   fi
