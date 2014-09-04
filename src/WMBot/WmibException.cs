6d22c1bedc79db45ad665caef9c233edef166284
Petr Bena <benapetr@gmail.com>
2014-09-04 14:22:38 +0200

    missing file

---
+++ src/WMBot/WmibException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wmib
+{
+    public class WmibException : Exception
+    {
+        public WmibException()
+        {
+        }
+
+        public WmibException(string message)
+            : base(message)
+        {
+        }
+
+        public WmibException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
