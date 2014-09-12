83f53289c7a0e8011395086ca5a5d092d6d142c3
Petr Bena <benapetr@gmail.com>
2014-09-12 14:33:01 +0200

    updated notify plugin

--- src/WMBot.Plugins/Notify/NotifyUs.cs
+++ src/WMBot.Plugins/Notify/NotifyUs.cs
@@ -19,12 +19,20 @@ namespace wmib.Extensions
     {
         public override bool Construct()
         {
-            Version = new Version(1, 0, 18, 0);
+            Version = new Version(1, 2, 0, 0);
             return true;
         }
 
         public override bool Hook_OnUnload()
         {
+            CommandPool.UnregisterCommand("notify");
+            lock (Notification.NotificationList)
+            {
+                foreach (Notification nt in Notification.NotificationList)
+                    Log("Dropping undelivered notification for user " + nt.Source_Name + " who was waiting for " + nt.User);
+
+                Notification.NotificationList.Clear();
+            }
             Core.Help.Unregister("notify");
             return true;
         }
@@ -158,11 +166,11 @@ namespace wmib.Extensions
                 }
                 result = Notification.RetrieveTarget(invoker.Nick);
             }
+        }
 
-            if (message.StartsWith(Configuration.System.CommandPrefix + "notify "))
-            {
-                this.NotifyUser(message, invoker, channel);
-            }
+        public void cmNotify(CommandParams pm)
+        {
+            this.NotifyUser(pm.Message, pm.User, pm.SourceChannel);
         }
 
         public override void Hook_Quit(libirc.UserInfo user, string Message)
@@ -196,9 +204,8 @@ namespace wmib.Extensions
             }
 
             if (message.StartsWith(Configuration.System.CommandPrefix + "notify "))
-            {
                 this.NotifyUser(message, user, user);
-            }
+
             return false;
         }
 
@@ -206,8 +213,6 @@ namespace wmib.Extensions
         {
             if (notification.Message == null)
                 return;
-
-
             IRC.DeliverMessage("Hi " + notification.User + ", " + notification.Source_Name + " was looking for you and wanted me to tell you this when you are here: " +
                                   notification.Message, notification.User);
         }
@@ -219,6 +224,7 @@ namespace wmib.Extensions
 
         public override bool Hook_OnRegister()
         {
+            CommandPool.RegisterCommand(new GenericCommand("notify", this.cmNotify));
             Core.Help.Register("notify", "inform you when specified user become active or join some channel in private message");
             return true;
         }
