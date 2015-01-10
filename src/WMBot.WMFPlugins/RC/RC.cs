a1086a9f677b62fa9fcf5acf00a4915709985e64
Petr Bena <benapetr@gmail.com>
2015-01-10 23:25:59 +0100

    fixed random crash
    I am not really sure why it happened, and this fix is not really a best one, but it should do until RC is completely rewritten

--- src/WMBot.WMFPlugins/RC/RC.cs
+++ src/WMBot.WMFPlugins/RC/RC.cs
@@ -373,33 +373,28 @@ namespace wmib
         /// </summary>
         public void Save()
         {
-            int line = 0;
             string dbn = Variables.ConfigurationDirectory + "/" + channel.Name + ".list";
             try
             {
                 string content = "";
-                line++;
                 Core.BackupData(dbn);
-                line++;
                 lock (MonitoredPages)
                 {
-                line++;
                     foreach (IWatch values in MonitoredPages)
                     {
+                        if (values == null || values.URL == null || values.Page == null || values.Channel == null)
+                            continue;
                         content = content + values.URL.name + "|" + values.Page.Replace("|", "<separator>") + "|" +
                                   values.Channel + "\n";
                     }
                 }
-                line++;
                 File.WriteAllText(dbn, content);
-                line++;
                 File.Delete(Configuration.TempName(dbn));
-                line++;
             }
             catch (Exception er)
             {
                 ModuleRC.ptrModule.Log("Error while saving to: " + channel.Name + ".list");
-                Core.HandleException(er, "RC (" + line + ")");
+                Core.HandleException(er, "RC");
                 Core.RecoverFile(dbn, channel.Name);
             }
         }
