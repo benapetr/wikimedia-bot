545628a20161ede1eae856f8442fd7e257b7efe6
Petr Bena <benapetr@gmail.com>
2015-03-02 09:24:02 -0500

    license fix

--- src/WMBot.Plugins/GitHub/listener/github.php
+++ src/WMBot.Plugins/GitHub/listener/github.php
@@ -1,3 +1,4 @@
+<?
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or   
@@ -13,7 +14,6 @@
 //  Free Software Foundation, Inc.,                                     
 //  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-<?
 class Git_Commit
 {
     public $commit_id;
--- src/WMBot.Plugins/GitHub/listener/irc.php
+++ src/WMBot.Plugins/GitHub/listener/irc.php
@@ -1,3 +1,4 @@
+<?
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation; either version 2 of the License, or   
@@ -12,8 +13,6 @@
 //  along with this program; if not, write to the                       
 //  Free Software Foundation, Inc.,                                     
 //  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-<?
 
 class IRC
 {
