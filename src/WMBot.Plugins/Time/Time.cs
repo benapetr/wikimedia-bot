5d4437a54b54a9e6bb9f9f5cc412017bc2ca3540
Andrew H <crazy4sb@gmail.com>
2016-01-02 15:39:18 -0500

    Add extension for @time command

---
+++ src/WMBot.Plugins/Time/Time.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace wmib.Extensions
+{
+	public class Time : Module
+	{
+		public override bool Hook_OnRegister()
+		{
+			RegisterCommand(new GenericCommand("time", this.time));
+			return base.Hook_OnRegister();
+		}
+
+		private void time(CommandParams p)
+		{
+			DateTime time = DateTime.UtcNow;
+			IRC.DeliverMessage(p.User.Nick + ": It is is " + time + " UTC", p.SourceChannel);
+		}
+
+		public override bool Hook_OnUnload()
+		{
+			UnregisterCommand("time");
+			return base.Hook_OnUnload();
+		}
+
+		public override bool Construct()
+		{
+			this.HasSeparateThreadInstance = false;
+			return true;
+		}
+	}
+}
+
