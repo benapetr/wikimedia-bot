fed38e5db330c3f7f1f9c3ace3e0fdbc2cb0c260
Petr Bena <benapetr@gmail.com>
2019-01-06 22:39:49 +0100

    updated

--- src/WMBot.Plugins/AI/AI.cs
+++ src/WMBot.Plugins/AI/AI.cs
@@ -21,8 +21,35 @@ using System.Threading;
 
 namespace wmib.Extensions
 {
+    // This whole module is a simple stupid attempt to give a bot some basic ability to communicate with others
     public class AI : Module
     {
+        private Dictionary<string, string> AutoResponses = new Dictionary<string, string>()
+        {
+            { "how are you",   "I am a robot, so as long as the power is on, I am fine :)" },
+            { "how are you doing",   "I am a robot, so as long as the power is on, I am fine :)" },
+            { "how do you do", "I am a robot, so as long as the power is on, I am doing just fine..." },
+            { "howdy", "howdy!" },
+            { "hello", "hi! I am a robot :)" },
+            { "hi", "hi! I am a robot :)" },
+            { "hey", "hi! I am a robot :)" },
+            { "who are you",  "I am a robot, I am here to help you when all humans are too busy" },
+            { "what are you", "I am a robot, I am here to help you when all humans are too busy" },
+            { "what are you doing", "I am waiting for a command :)" },
+            { "can you help me", "I am just a stupid robot, you can try to search my infobot db (use @search <keyword>) for an answer" },
+            { "can you help",    "I am just a stupid robot, you can try to search my infobot db (use @search <keyword>) for an answer" }
+        };
+
+        private Dictionary<string, string> autoQuestionResponses = new Dictionary<string, string>()
+        {
+            { "my nick", "Your nick is $invoker.nick" },
+            { "my ident", "Your ident is $invoker.ident" },
+            { "my host", "Your hostname is $invoker.host" },
+            { "my hostname", "Your hostname is $invoker.host" }
+        };
+
+        private List<string> questionStarts = new List<string>() { "can you tell me ", "could you tell me ", "can I ask ", "what is ", "what's " };
+
         public AI()
         {
         }
@@ -74,6 +101,93 @@ namespace wmib.Extensions
             p.SourceChannel.SaveConfig();
         }
 
+        private string NoSpecials(string text)
+        {
+            text = text.Replace(",", " ").Replace("!", " ").Replace(".", " ").Replace("?", " ");
+            while (text.Contains("  "))
+                text = text.Replace("  ", " ");
+            if (text.StartsWith(" ", StringComparison.InvariantCulture))
+                text = text.Substring(1);
+
+            if (text.EndsWith(" ", StringComparison.InvariantCulture))
+                text = text.Substring(0, text.Length - 1);
+
+            return text;
+        }
+
+        private void Respond(Channel channel, libirc.UserInfo invoker, string message)
+        {
+            message = message.Replace("$invoker.nick", invoker.Nick)
+                             .Replace("$invoker.host", invoker.Host)
+                             .Replace("$invoker.ident", invoker.Ident);
+
+            IRC.DeliverMessage(invoker.Nick + ": " + message, channel);
+        }
+
+        private bool processQuestion(Channel channel, libirc.UserInfo invoker, string question)
+        {
+            if (question.StartsWith(" "))
+                question = question.Substring(1);
+
+            if (question.StartsWith("what is "))
+                question = question.Substring("what is ".Length);
+
+            if (autoQuestionResponses.ContainsKey(question))
+            {
+                this.Respond(channel, invoker, autoQuestionResponses[question]);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ProcessInput(Channel channel, libirc.UserInfo invoker, string message)
+        {
+            string l_message = message.ToLower();
+            string ns_message = NoSpecials(message);
+
+            if (this.AutoResponses.ContainsKey(ns_message))
+            {
+                this.Respond(channel, invoker, this.AutoResponses[ns_message]);
+                return;
+            }
+
+            bool start_with_question = false;
+            foreach (string question in this.questionStarts)
+            {
+                if (ns_message.StartsWith(question, StringComparison.InvariantCulture))
+                {
+                    start_with_question = true;
+                    break;
+                }
+            }
+
+            if (start_with_question || l_message.EndsWith("?", StringComparison.InvariantCulture))
+            {
+                string question = ns_message;
+                if (start_with_question)
+                {
+                    foreach (string qs in this.questionStarts)
+                    {
+                        if (question.StartsWith(qs, StringComparison.InvariantCulture))
+                        {
+                            question.Substring(qs.Length);
+                            break;
+                        }
+                    }
+                }
+                if (question.EndsWith("?", StringComparison.InvariantCulture))
+                {
+                    question = question.Substring(0, question.Length - 1);
+                }
+
+                if (this.processQuestion(channel, invoker, question))
+                    return;
+            }
+
+            this.Respond(channel, invoker, "Sorry, but I don't know to respond to this. Please keep in mind I am just a stupid robot, I can't hold an intelligent conversation.");
+        }
+
         public override void Hook_PRIV(Channel channel, libirc.UserInfo invoker, string message)
         {
             if (!GetConfig(channel, "AI.Enabled", false))
@@ -85,7 +199,7 @@ namespace wmib.Extensions
 
             message = message.Substring(channel.PrimaryInstance.Nick.Length + 2);
 
-
+            this.ProcessInput(channel, invoker, message);
         }
     }
 }
